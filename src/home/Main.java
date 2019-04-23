9a3e04e7b5bb835769da0dd9ca115b0cf8f8383d
6eJlo4Ka <mohctpuk9674@gmail.com>
2019-04-23 21:08:43 +0300

    21

--- src/home/Main.java
+++ src/home/Main.java
@@ -1,4 +1,14 @@
 package home;
 
+import home.prod.Animal;
+import home.prod.Food;
+import home.prod.Home;
+
 public class Main {
+    Product product1=new Food("meat",23);
+    Product product2=new Home("fork", 20);
+    Product product3=new Animal("plate", 21);
+
+
+
 }
